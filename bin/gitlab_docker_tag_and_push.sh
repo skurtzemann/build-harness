18a97b9c32cd7b41b1d4da14b22f6f16cda59d6e
Sébastien Kurtzemann <sebastien.k@capkopper.fr>
2017-11-29 15:03:21 +0100

    Add more docker tags for the GitLab CI build image

--- bin/gitlab_docker_tag_and_push.sh
+++ bin/gitlab_docker_tag_and_push.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 
-DOCKER_TAGS=("$CI_COMMIT_SHA")
-DOCKER_TAGS+=("${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHA:0:8}")
-
 echo "CI_JOB_ID=${CI_JOB_ID}"
 
+# We create several docker tags to identified to build container
+# - the full commit SHA
+# - the branch or tag
+# - the branch or tag with the 'short' commit SHA (8 characters)
+# - the branch or tag with the GitLab CI job id 
+DOCKER_TAGS=("$CI_COMMIT_SHA")
+DOCKER_TAGS+=("$CI_COMMIT_REF_NAME")
+DOCKER_TAGS+=("${CI_COMMIT_REF_NAME}-${CI_COMMIT_SHA:0:8}")
+DOCKER_TAGS+=("${CI_COMMIT_REF_NAME}-${CI_JOB_ID}")
+
+# For a git tag create an additionnal docker tag
 if [ ! -z "${CI_COMMIT_TAG}" ]; then
   echo "CI_COMMIT_TAG=${CI_COMMIT_TAG}"
   DOCKER_TAGS+=("${CI_COMMIT_TAG}")
 fi
 
+# For the master branch add the "latest" tag
+if [ "${CI_COMMIT_REF_NAME}" == "master" ]; then
+  DOCKER_TAGS+=("latest")
+fi
+
+# For each 'DOCKER_TAGS' item tag and push an docker image based on the tag "ci-build" 
 for TAG in "${DOCKER_TAGS[@]}"; do
   echo "Tagging ${DOCKER_REGISTRY_SERVER}/${DOCKER_IMAGE}:${TAG}"
   docker tag "${DOCKER_IMAGE}:ci-build" "${DOCKER_REGISTRY_SERVER}/${DOCKER_IMAGE}:${TAG}" && \
